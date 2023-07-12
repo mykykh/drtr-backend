b48af80d8c9384429aa3b6d0a269d31e6de0e8b4
mykykh <49101849+mykykh@users.noreply.github.com>
2023-07-12 20:19:28 +0300

    feat: add typescript and hardhat-deploy

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import 'hardhat-deploy'
+import { HardhatUserConfig } from 'hardhat/types';
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
